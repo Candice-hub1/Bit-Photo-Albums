8e13a41df814f82203449baeb214810fb8a4401d
Candice-hub1 <candicenompumelelo1@gmail.com>
2021-11-23 02:26:57 +0200

    Added project files

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BIT__PHOTO_ALBUMS.Models;
+
+namespace BIT__PHOTO_ALBUMS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BIT__PHOTO_ALBUMS.Models.Photo> Photo { get; set; }
+    }
+}
